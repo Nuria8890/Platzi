a5337df74501e4f094e69f330f5dc2c7c209c8c7
Nuria Campo <nuria.campo7@gmail.com>
2024-08-21 11:45:37 +0200

    Mensaje ToDo list completada

--- curso-React/curso-react-intro/src/components/TodoCounter.js
+++ curso-React/curso-react-intro/src/components/TodoCounter.js
@@ -2,9 +2,15 @@ import '../css/TodoCounter.css';
 
 function TodoCounter({completed, total}) {
   return(
-    <h1 className='TodoCounter'>
-      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
-    </h1>
+    completed === total
+      ?
+        <h1 className='TodoCounter'>
+          Enhorabuenaaa!!! Completaste tu ToDo list!!! YUHHUUUU!!!!
+        </h1>
+      :
+        <h1 className='TodoCounter'>
+          Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+        </h1>
   );
 }
 
